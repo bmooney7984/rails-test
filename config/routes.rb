ac62f256a72131f66f9001070284831103a53ab4
Brendan Mooney <bmooney7984@gmail.com>
2022-04-15 16:04:25 -0400

    updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
+# As per Odin instructions:
 Rails.application.routes.draw do
+  root 'cars#index'
   resources :cars
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
+
+# Original file:
+#Rails.application.routes.draw do
+#  resources :cars
+#  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#
+#  # Defines the root path route ("/")
+#  # root "articles#index"
+#end
